1dfcbcf0adb7725ce6287e1a96de0bded62c8f24
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-29 14:33:07 +0200

    finish review store

--- src/store/storeReview.js
+++ src/store/storeReview.js
@@ -1,1 +1,31 @@
-//comment
+import create from "zustand";
+import { nanoid } from "nanoid";
+
+const useReviewStore = create((set) => ({
+  reviews: [
+    { id: 1, content: "hello" },
+    { id: 2, content: "second" },
+    { id: 3, content: "hello" },
+  ],
+  createReview: (content) =>
+    set((state) => ({
+      reviews: [...state.reviews, { id: nanoid(), content }],
+    })),
+  deleteReview: (reviewId) =>
+    set((state) => ({
+      reviews: state.reviews.filter((review) => review.id !== reviewId),
+    })),
+  updateReview: (reviewId, content) =>
+    set((state) => ({
+      reviews: state.reviews.map((review) =>
+        review.id === reviewId
+          ? {
+              ...review,
+              content,
+            }
+          : review
+      ),
+    })),
+}));
+
+export default useReviewStore;
