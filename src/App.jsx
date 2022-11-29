3c5d539d294e798d6ef63910b8abd820360b3063
Yerjan <yerjan@proton.me>
2022-11-29 13:56:23 +0300

    add nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import useMovieStore from "./store/storeMovies.js";
 import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const movies = useMovieStore((state) => state.movies);
-  const addReview = useMovieStore((state) => state.addReviewToMovie);
-  const removeReview = useMovieStore((state) => state.removeReviewFromMovie);
+	const movies = useMovieStore((state) => state.movies);
+	const addReview = useMovieStore((state) => state.addReviewToMovie);
+	const removeReview = useMovieStore((state) => state.removeReviewFromMovie);
 
-  return (
-    <div>
-      {movies.map((movie) => {
-        return (
-          <h1 key={movie.id}>
-            MovieID:{movie.id}, Reviews:
-            {movie.reviews.map((review) => {
-              return (
-                <div key={review}>
-                  <p>{review}</p>
-                  <button onClick={() => removeReview(movie.id, review)}>
-                    Delete
-                  </button>
-                </div>
-              );
-            })}
-          </h1>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div>
+			{movies.map((movie) => {
+				return (
+					<h1 key={movie.id}>
+						MovieID:{movie.id}, Reviews:
+						{movie.reviews.map((review) => {
+							return (
+								<div key={review}>
+									<p>{review}</p>
+									<button onClick={() => removeReview(movie.id, review)}>
+										Delete
+									</button>
+								</div>
+							);
+						})}
+					</h1>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
