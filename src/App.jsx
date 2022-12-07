5b3244a3d548690721e2fa9b1bb724bd8b2e056e
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-12-07 09:42:20 +0200

    done home page mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-			<Outlet />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/store/storeMovies.js
+++ src/store/storeMovies.js
@@ -1,30 +1,27 @@
 import create from "zustand";
 // import initMovies from "./initMovies";
-
+import { INIT_MOVIES } from "./initMovies";
 const useMovieStore = create((set) => ({
-	movies: [
-		{ id: "1", reviews: ["hello", "lkjleig"] },
-		{ id: "2", reviews: ["inoin"] },
-	],
-	addReviewToMovie: (movieId, reviewId) =>
-		set((state) => ({
-			movies: state.movies.map((movie) =>
-				movie.id === movieId
-					? { ...movie, reviews: [...movie.reviews, reviewId] }
-					: movie,
-			),
-		})),
-	removeReviewFromMovie: (movieId, reviewId) =>
-		set((state) => ({
-			movies: state.movies.map((movie) =>
-				movie.id === movieId
-					? {
-							...movie,
-							reviews: movie.reviews.filter((review) => review !== reviewId),
-					  }
-					: movie,
-			),
-		})),
+  movies: INIT_MOVIES,
+  addReviewToMovie: (movieId, reviewId) =>
+    set((state) => ({
+      movies: state.movies.map((movie) =>
+        movie.id === movieId
+          ? { ...movie, reviews: [...movie.reviews, reviewId] }
+          : movie
+      ),
+    })),
+  removeReviewFromMovie: (movieId, reviewId) =>
+    set((state) => ({
+      movies: state.movies.map((movie) =>
+        movie.id === movieId
+          ? {
+              ...movie,
+              reviews: movie.reviews.filter((review) => review !== reviewId),
+            }
+          : movie
+      ),
+    })),
 }));
 
 export default useMovieStore;
