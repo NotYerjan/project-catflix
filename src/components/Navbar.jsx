7ef02b0d46c301c0442378b6971a0ab4a010c67d
Suleyman <suleyman20071998@gmail.com>
2022-12-07 16:46:30 +0200

    created navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { FiHeart, FiHome, FiSearch, FiUser } from "react-icons/fi";
+import "./NavBar.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navBar">
+      <ul className="navBarContent">
+        <li>
+          <Link to="/movies"><FiHome  className="navBarIcon"/></Link>
+        </li>
+        <li>
+          <Link to="/liked"><FiHeart  className="navBarIcon"/></Link>
+        </li>
+        <li>
+          <Link to="/users"><FiUser   className="navBarIcon"/></Link>
+        </li>
+        <li>
+          <Link to="/search"><FiSearch   className="navBarIcon"/></Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
